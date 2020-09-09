280bdbb7d52fbe2c6670306373efaec453dc6ef9
DimasIlham30 <70867817+DimasIlham30@users.noreply.github.com>
2020-09-09 15:20:03 +0700

    Add files via upload

---
+++ PERTEMUAN 2 TEORI/Model/Dosen.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Dosen {
+
+    /**
+     * Default constructor
+     */
+    public Dosen() {
+    }
+
+    /**
+     * 
+     */
+    public String nama;
+
+    /**
+     * 
+     */
+    public String matkul;
+
+
+}
---
+++ PERTEMUAN 2 TEORI/Model/Mahasiswa.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Mahasiswa {
+
+    /**
+     * Default constructor
+     */
+    public Mahasiswa() {
+    }
+
+    /**
+     * 
+     */
+    public int Nim;
+
+    /**
+     * 
+     */
+    public int nilai;
+
+    /**
+     * 
+     */
+    public String nama;
+
+}
